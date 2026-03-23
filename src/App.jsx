0d07309b9959ccebb40f791e5d4ffdb937a4d87f
Nishba Ali <alinishba@gmail.com>
2026-03-23 22:02:06 +0000

    Update App.jsx
    Testing safety status, adding imports through comments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
-
-
+import SafetyStatus from './components/SafetyStatus'
+/*import WeatherMetrics from './components/WeatherMetrics'
+import WeatherGraph from './components/WeatherGraph'
+import WeekForecast from './components/WeekForecast'
+import Dashboard from './components/Dashboard'
+*/
 function App() {
- return (
- <>
- Hello!
- </>
- )
-}
+    return (
+      <>
+        <SafetyStatus 
+          windspeed={14}
+          gustspeed={22}
+          visibility={8}
+        />
+      </>
+    )
+  }
+  
 
 export default App
